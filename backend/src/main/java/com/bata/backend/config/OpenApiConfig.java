678edb93216d48096b9800d9a00f7af83a96331e
fernandoOS03 <fernando.192023@outlook.es>
2025-12-16 07:45:14 -0500

    feat : implementación del botón Authorize botón Authorize en Swagger

---
+++ backend/src/main/java/com/bata/backend/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.bata.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(
+		info = @Info(
+			title = "Bata backend API", 
+			version = "1.0", 
+			description = "Documentación oficial de la API de Bata"
+		), 
+		security = @SecurityRequirement(name = "bearerAuth") //aplica seguridad globalmente
+		)
+@SecurityScheme(
+		name = "bearerAuth",
+		description = "JWT de autenticación",
+		scheme = "bearer", //tipo dee esquema 
+		type = SecuritySchemeType.HTTP, 
+		bearerFormat = "JWT", //formato del token
+		in = SecuritySchemeIn.HEADER) // donde se envia
+public class OpenApiConfig {
+
+}
