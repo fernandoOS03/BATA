38cc4a79bf93bdd720c640a7482119fa7516108a
fernandoOS03 <fernando.192023@outlook.es>
2025-12-16 07:41:03 -0500

    feat : se añade controller user con algunos endpoint básicos

---
+++ backend/src/main/java/com/bata/backend/modules/user/controllers/UserController.java
@@ -0,0 +1,44 @@
+package com.bata.backend.modules.user.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bata.backend.modules.user.dto.request.UserLoginRequest;
+import com.bata.backend.modules.user.dto.request.UserRegisterRequest;
+import com.bata.backend.modules.user.dto.response.UserLoginResponse;
+import com.bata.backend.modules.user.dto.response.UserResponse;
+import com.bata.backend.modules.user.services.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+//http://localhost:8080/swagger-ui.html
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+	private final UserService userService;
+
+	@PostMapping("/register")
+	public ResponseEntity<UserResponse> register(@RequestBody @Valid UserRegisterRequest request) {
+		return ResponseEntity.ok(userService.createUser(request));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<UserResponse>> getAll() {
+	    return ResponseEntity.ok(userService.getAllUsers());
+	}
+	
+	@PostMapping("/login")
+    public ResponseEntity<UserLoginResponse> login(@RequestBody @Valid UserLoginRequest request) {
+        return ResponseEntity.ok(userService.login(request));
+    }
+
+}
