1ef675482d39f0a8552e3c4849d174e3af23af60
fernandoOS03 <fernando.192023@outlook.es>
2025-12-16 23:36:14 -0500

    feat :implementación del servicio y controller para la entidad producto

---
+++ backend/src/main/java/com/bata/backend/modules/product/controllers/ProductController.java
@@ -0,0 +1,40 @@
+package com.bata.backend.modules.product.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bata.backend.modules.product.dto.request.ProductRequest;
+import com.bata.backend.modules.product.dto.response.ProductResponse;
+import com.bata.backend.modules.product.services.ProductService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+	private final ProductService productService;
+	
+	// ==========================================================
+	//1. ENDPOINT PÚBLICO : Para ver el catálogo
+	// ==========================================================
+	@GetMapping
+	public ResponseEntity<List<ProductResponse>> getAll(){
+		 return ResponseEntity.ok(productService.getAllProducts());
+	 }
+	
+	// ==========================================================
+	//1. ENDPOINT PRIVADO : Crear producto (solo admin)
+	// ==========================================================
+	@PostMapping
+	public ResponseEntity<ProductResponse> create(@Valid @RequestBody ProductRequest request){
+		return ResponseEntity.ok(productService.createProduct(request));
+	}
+}
---
+++ backend/src/main/java/com/bata/backend/modules/product/services/ProductService.java
@@ -0,0 +1,117 @@
+package com.bata.backend.modules.product.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.bata.backend.modules.product.dto.request.ProductRequest;
+import com.bata.backend.modules.product.dto.response.ProductResponse;
+import com.bata.backend.modules.product.entities.BrandEntity;
+import com.bata.backend.modules.product.entities.CategoryEntity;
+import com.bata.backend.modules.product.entities.ColorEntity;
+import com.bata.backend.modules.product.entities.MaterialEntity;
+import com.bata.backend.modules.product.entities.ProductEntity;
+import com.bata.backend.modules.product.entities.ProductVariantEntity;
+import com.bata.backend.modules.product.entities.SizeEntity;
+import com.bata.backend.modules.product.mapper.ProductMapper;
+import com.bata.backend.modules.product.repositories.BrandRepository;
+import com.bata.backend.modules.product.repositories.CategoryRepository;
+import com.bata.backend.modules.product.repositories.ColorRepository;
+import com.bata.backend.modules.product.repositories.MaterialRepository;
+import com.bata.backend.modules.product.repositories.ProductRepository;
+import com.bata.backend.modules.product.repositories.SizeRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+	private final ProductRepository productRepository;
+	private final BrandRepository brandRepository;
+	private final CategoryRepository categoryRepository;
+	private final MaterialRepository materialRepository;
+	private final ColorRepository colorRepository;
+	private final SizeRepository sizeRepository;
+
+	private final ProductMapper productMapper;
+
+	// =============================================================
+	// METODO PARA LISTAR TODOS (Para el catálogo público)
+	// =============================================================
+	public List<ProductResponse> getAllProducts() {
+		return productRepository.findAll().stream().map(productMapper::toDto).collect(Collectors.toList());
+	}
+
+	// =============================================================
+	// METODO PARA CREAR UN PRODUCTO CON SUS ENTIDADES
+	// =============================================================
+	@Transactional // si algo falla no guarda nada(Rooback)
+	public ProductResponse createProduct(ProductRequest request) {
+		// PASO 1: Convertir el request a Entidad base
+		ProductEntity product = productMapper.toEntity(request);
+
+		// PASO 2: Buscar y asignar las relaciones "Padre" (Marca, Categoría, Material)
+		// Si no existen, lanzamos error para avisar al admin.
+		BrandEntity brand = brandRepository.findById(request.brandId())
+				.orElseThrow(() -> new RuntimeException("Marca no encontrada con ID: " + request.brandId()));
+
+		CategoryEntity category = categoryRepository.findById(request.categoryId())
+				.orElseThrow(() -> new RuntimeException("Categoría no encontrada con ID: " + request.categoryId()));
+
+		MaterialEntity material = materialRepository.findById(request.materialId())
+				.orElseThrow(() -> new RuntimeException("Material no encontrado con ID: " + request.materialId()));
+
+		product.setBrand(brand);
+		product.setCategory(category);
+		product.setMaterial(material);
+
+		// PASO 3: Procesar la lista de Variantes (Hijos)
+		// Creamos la lista vacía donde guardaremos las variantes procesadas
+		List<ProductVariantEntity> variantsList = new ArrayList<>();
+
+		// Recorremos cada variante que envió el usuario
+		for (ProductRequest.ProductVariantRequest variantReq : request.variants()) {
+
+			ProductVariantEntity variantEntity = new ProductVariantEntity();
+
+			// a) Copiar datos simples
+			variantEntity.setStock(variantReq.stock());
+			variantEntity.setPriceModifier(variantReq.priceModifier());
+			variantEntity.setImagenUrl(variantReq.imageUrl()); // Asumiendo que agregaste este campo
+
+			// b) Buscar Color y Talla
+			ColorEntity color = colorRepository.findById(variantReq.colorId())
+					.orElseThrow(() -> new RuntimeException("Color no encontrado ID: " + variantReq.colorId()));
+
+			SizeEntity size = sizeRepository.findById(variantReq.sizeId())
+					.orElseThrow(() -> new RuntimeException("Talla no encontrada ID: " + variantReq.sizeId()));
+
+			variantEntity.setColor(color);
+			variantEntity.setSize(size);
+
+			// c) VINCULACIÓN BIDIRECCIONAL (Clave para que funcione el Cascade)
+			//Esto es obligatorio en JPA porque obliga a que las llaves foraneas se guarden correctamente
+			// Le decimos al hijo quién es su padre
+			variantEntity.setProduct(product);
+
+			// Agregamos a la lista temporal
+			variantsList.add(variantEntity);
+		}
+
+		// Le decimos al padre quiénes son sus hijos
+		product.setProductsVariants(variantsList);
+
+		// PASO 4: Guardar todo de un golpe
+		// Gracias a CascadeType.ALL, al guardar 'product' se guardan solas las
+		// variantes
+		ProductEntity savedProduct = productRepository.save(product);
+
+		// PASO 5: Convertir a DTO de respuesta y retornar
+		return productMapper.toDto(savedProduct);
+
+	}
+
+}
