ef5d54d222a564a280d993a32311c8d3887eba05
fernandoOS03 <fernando.192023@outlook.es>
2025-12-18 00:43:29 -0500

    feat : se implementa contrato findByUserId

--- backend/src/main/java/com/bata/backend/modules/order/repositories/OrderRepository.java
+++ backend/src/main/java/com/bata/backend/modules/order/repositories/OrderRepository.java
@@ -1,5 +1,7 @@
 package com.bata.backend.modules.order.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.bata.backend.modules.order.entities.OrderEntity;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Integer>{
-
+ 
+	//Para mostar los pedidos del usuario
+	List<OrderEntity> findByUserId (Integer userId);
 }
