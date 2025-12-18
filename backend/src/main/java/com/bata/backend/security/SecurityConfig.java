1ebcc9fbb74e7ab6700764119a4d9e16c2318d11
fernandoOS03 <fernando.192023@outlook.es>
2025-12-18 18:59:02 -0500

    feat :  @bean para el manejo de los cors

--- backend/src/main/java/com/bata/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/bata/backend/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.bata.backend.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -8,11 +10,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.bata.backend.security.jwt.JwtAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
-
 
 /*
  * Cierra todas las puertas excepto login/registro.
@@ -23,28 +27,44 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-	private final JwtAuthenticationFilter jwtAuthFilter;     // Inyectar Filtro
-    private final AuthenticationProvider authenticationProvider; // Inyectar Provider
+	private final JwtAuthenticationFilter jwtAuthFilter; // Inyectar Filtro
+	private final AuthenticationProvider authenticationProvider; // Inyectar Provider
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                .requestMatchers("/api/users/register", "/api/users/login").permitAll()
-                .requestMatchers(org.springframework.http.HttpMethod.GET, "/api//pruducts").permitAll()
-                .requestMatchers(org.springframework.http.HttpMethod.POST, "/api/products").hasRole("ADMIN")
-                .requestMatchers("/api/orders/**").authenticated() //solo usuaris autenticados pueden comprar
-                .anyRequest().authenticated() // ¡Todo lo demás cerrado!
-            )
-            // IMPORTANTE: Gestión de sesión STATELESS (Sin estado)
-            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authenticationProvider(authenticationProvider)
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // <--- AQUÍ SE AGREGA EL FILTRO
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(
+						auth -> auth.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+								.permitAll().requestMatchers("/api/users/register", "/api/users/login").permitAll()
+								.requestMatchers(org.springframework.http.HttpMethod.GET, "/api//pruducts").permitAll()
+								.requestMatchers(org.springframework.http.HttpMethod.POST, "/api/products")
+								.hasRole("ADMIN").requestMatchers("/api/orders/**").authenticated() // solo usuaris
+																									// autenticados
+																									// pueden comprar
+								.anyRequest().authenticated() // ¡Todo lo demás cerrado!
+				)
+				// IMPORTANTE: Gestión de sesión STATELESS (Sin estado)
+				.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authenticationProvider(authenticationProvider)
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // <--- AQUÍ SE AGREGA EL
+																								// FILTRO
 
-        return http.build();
-    }
-	
+		return http.build();
+	}
+
+	//------ PARA LOS CORS ------
+	@Bean
+	public CorsConfigurationSource configurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	};
 
 }
