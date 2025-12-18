40fa700791d50e463deab02adcabe8b57d49671a
fernandoOS03 <fernando.192023@outlook.es>
2025-12-18 00:42:29 -0500

    feat : controller para el modulo order

---
+++ backend/src/main/java/com/bata/backend/modules/order/controller/OrderController.java
@@ -0,0 +1,39 @@
+package com.bata.backend.modules.order.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bata.backend.modules.order.services.OrderService;
+import com.bata.backend.modules.order.dto.request.OrderRequest;
+import com.bata.backend.modules.order.dto.response.OrderResponse;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestBody;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+	private final OrderService orderService;
+
+	@PostMapping
+	public ResponseEntity<OrderResponse> create(@RequestBody @Valid OrderRequest request,
+			Authentication authentication) {	
+		String userEmail = authentication.getName();
+		return ResponseEntity.ok(orderService.createOrder(request, userEmail));
+	}
+	
+	@GetMapping("/my-orders")
+	public ResponseEntity<List<OrderResponse>> getMyOrders (Authentication authentication){
+		String userEmail = authentication.getName();
+		return ResponseEntity.ok(orderService.getMyOrders(userEmail));
+	}
+
+}
