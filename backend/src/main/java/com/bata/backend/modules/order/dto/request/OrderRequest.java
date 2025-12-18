7ad6eade1d28e673679e67b555dff24fdb66b914
fernandoOS03 <fernando.192023@outlook.es>
2025-12-18 00:42:05 -0500

    feat : se agrega contrato findByUserId para mostar los pedidos del user

--- backend/src/main/java/com/bata/backend/modules/order/dto/request/OrderRequest.java
+++ backend/src/main/java/com/bata/backend/modules/order/dto/request/OrderRequest.java
@@ -2,21 +2,24 @@ package com.bata.backend.modules.order.dto.request;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record OrderRequest(
 		@NotNull(message = "La dirección de envío es obligatoria") 
+		@JsonProperty("addressId")
 		Integer addressId, 
 																											// direcció																											// usuari
 		@NotNull(message = "El método de pago es obligatorio") 
+		@JsonProperty("paymentMethod")
 		String paymentMethod, 
 
 		@NotEmpty(message = "El carrito no puede estar vacío") 
+		@JsonProperty("items")
 		List<OrderItemRequest> items 
+		
+		
 ) {
-	public record OrderItemRequest(	
-			@NotNull Integer variantId, 
-			@NotNull Integer quantity) {
-	}
 }
