ed56668c77648810a2f14a4d9730238767db17ac
fernandoOS03 <fernando.192023@outlook.es>
2025-12-18 00:37:27 -0500

    refactor : cambio de ubicación del contrato findByEmail

--- backend/src/main/java/com/bata/backend/modules/user/repositories/LoginRepository.java
+++ backend/src/main/java/com/bata/backend/modules/user/repositories/LoginRepository.java
@@ -1,11 +1,13 @@
 package com.bata.backend.modules.user.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.bata.backend.modules.user.entities.LoginEntity;
 
 @Repository
-public interface LoginRepository extends JpaRepository<LoginEntity, Integer>{
-
+public interface LoginRepository extends JpaRepository<LoginEntity, Integer> {
+	Optional<LoginEntity> findByEmail(String email);
 }
--- backend/src/main/java/com/bata/backend/modules/user/repositories/UserRepository.java
+++ backend/src/main/java/com/bata/backend/modules/user/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.bata.backend.modules.user.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,4 @@ import com.bata.backend.modules.user.entities.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer>{
-
 }
