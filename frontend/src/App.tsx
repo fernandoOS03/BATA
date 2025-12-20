985e024d5a99144e9e836d5e08940fdea0600787
fernandoOS03 <fernando.192023@outlook.es>
2025-12-20 14:49:30 -0500

    feat se implementa enrrutador

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,17 @@
+import { BrowserRouter } from "react-router-dom"
+import { Toaster } from "sonner"
+import { AppRouter } from "./router/AppRouter"
+
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-100">
-      <div className="text-center p-10 bg-white shadow-xl rounded-xl">
-        <h1 className="text-5xl font-bold text-red-600 mb-4">
-          ¡Bata Shoes! 👟
-        </h1>
-        <p className="text-lg text-gray-600">
-          Si ves esto rojo y centrado, Tailwind funciona.
-        </p>
-        <div className="bg-white p-6 rounded-xl shadow-card hover:shadow-card-hover transition-shadow duration-300">
-          <h3>Zapatilla Bata</h3>
-        </div>
-        <button className="mt-6 px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
-          Comprar Ahora
-        </button>
-      </div>
-    </div>
+    //Habilita la nagevacion sin recargar la pagina 
+    <BrowserRouter>
+    {/*Esto decide que paginas mostrar en la url*/}
+      <AppRouter />
+      {/*Toaster es un componente que muestra notificaciones*/}
+      <Toaster position="top-center" richColors/>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
