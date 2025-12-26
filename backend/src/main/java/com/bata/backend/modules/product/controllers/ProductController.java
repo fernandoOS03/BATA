029e59f322e27a769ebfbade82c38343de12279e
fernandoOS03 <fernando.192023@outlook.es>
2025-12-26 17:48:43 -0500

    feat : se integra funcion para editar producto

--- backend/src/main/java/com/bata/backend/modules/product/controllers/ProductController.java
+++ backend/src/main/java/com/bata/backend/modules/product/controllers/ProductController.java
@@ -3,9 +3,11 @@ package com.bata.backend.modules.product.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,4 +49,20 @@ public class ProductController {
 	public ResponseEntity<ProductResponse> create(@Valid @RequestBody ProductRequest request){
 		return ResponseEntity.ok(productService.createProduct(request));
 	}
+	
+	// ==========================================================
+	//1. ENDPOINT PRIVADO : editar un producto
+	// ==========================================================
+	@PutMapping("/{id}")
+	public ResponseEntity<ProductResponse> update(@PathVariable Integer id, @RequestBody ProductRequest request){
+		return ResponseEntity.ok(productService.updateProduct(id, request));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		productService.deleteProduct(id);
+		return ResponseEntity.noContent().build();
+	}
+		
+	
 }
